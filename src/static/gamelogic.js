979fcd6fdb933dfd4931884b5f43ed9f17a81681
lincolnschick <lincolntschick@gmail.com>
2022-02-02 14:03:53 -0600

    Added comment for Battleship constructor

--- src/static/gamelogic.js
+++ src/static/gamelogic.js
@@ -3,6 +3,8 @@ const cols = 10;
 
 class Battleship {
     constructor(numShips) {
+        //Note: I initialized boards to all 0s. Figured we could represent empty cells as 0s, 
+        //cells where ships are with 1s, and cells that were hit with -1.
         this.board1 = Array(rows).fill().map(() => Array(cols).fill(0));
         this.board2 = Array(rows).fill().map(() => Array(cols).fill(0));
         this.hidden1 = Array(rows).fill().map(() => Array(cols).fill(0));
