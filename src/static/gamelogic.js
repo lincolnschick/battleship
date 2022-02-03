9acdecef0f8240152b7d71e6be4fd35d5bde3cc7
BayleyD <Bayley.D021001@gmail.com>
2022-02-02 21:31:54 -0600

    Created a gameBoard class
    It represents the board and has simple functions. Time: about 30 minutes.

--- src/static/gamelogic.js
+++ src/static/gamelogic.js
@@ -1,16 +1,56 @@
-const rows = 10;
-const cols = 10;
+
+/*----------------------------------------------------------------------------------------------------------------*/
+//A class that represents the gameBoard is created.
+//It contains only a 2D array and three functions which are self-explanitory 
+//Each cell in the board has multiple states...
+//-2 = ship was hit        -1 = empty space was hit
+// 0 = empty space          1 = ship in spot
+class gameBoard {
+    constructor(){
+        this.board = 
+            this.rows = 10;
+            this.cols = 10;
+            this.board = Array(this.rows).fill().map(() => Array(this.cols).fill(0));
+    }
+
+    //A function that simply fires at the given cell and adjusts state accordingly
+    firedAt( row, col ){
+        if ( this.board[ row ][ col ]  == 1 ){
+            this.board[ row ][ col ] = -2;
+        } else {
+            this.board[ row ][ col ] = -1;
+        }
+    }
+
+    //A function that adds a ship to the given cell
+    placeShip( row, col ){
+        if ( this.board[ row ][ col ] == 0 ){
+            this.board[ row ][ col ] = 1;
+        }
+    }
+
+    //Resets all the cells to 0
+    resetBoard(){
+        for ( let i = 0 ; i < this.rows ; i ++ ){
+            for ( let j = 0 ; j < this.cols ; j ++ ){
+                this.board[ i ][ j ] = 0;
+            }
+        }
+    }
+}
+/*----------------------------------------------------------------------------------------------------------------*/
 
 class Battleship {
     constructor(numShips) {
         //Note: I initialized boards to all 0s. Figured we could represent empty cells as 0s, 
         //cells where ships are with 1s, and cells that were hit with -1.
         //Hidden boards would not hold information about where ships are (only hits and empty)
-        this.board1 = Array(rows).fill().map(() => Array(cols).fill(0));
-        this.board2 = Array(rows).fill().map(() => Array(cols).fill(0));
-        this.hidden1 = Array(rows).fill().map(() => Array(cols).fill(0));
-        this.hidden2 = Array(rows).fill().map(() => Array(cols).fill(0));
-        this.numShips = numShips;
+            //Temporarily commenting out (keeping just in case)
+            /*this.board1 = Array(rows).fill().map(() => Array(cols).fill(0));
+            this.board2 = Array(rows).fill().map(() => Array(cols).fill(0));
+            this.hidden1 = Array(rows).fill().map(() => Array(cols).fill(0));
+            this.hidden2 = Array(rows).fill().map(() => Array(cols).fill(0));
+            this.numShips = numShips;*/
     }
     /** 
      * @param {number} board - 1 or 2
