bcb597c9099b0983460116eaf184372291d9e0a8
BayleyD <Bayley.D021001@gmail.com>
2022-02-07 16:34:18 -0600

    Testing a whole bunch of placement

--- src/static/battleship.js
+++ src/static/battleship.js
@@ -1,8 +1,8 @@
 let numberOfShips = 0;
 let backEnd = new Battleship(0);
 let playerOnePlacement = false;
-let shipOrientation = "Horizontal";
-let shipSelected = 0; 
+let shipOrientation = "Hl";
+let shipSelected = 5; 
 
 function moveToShipSelect() {
     document.getElementById("startmenu").style.visibility = "hidden";
@@ -13,11 +13,97 @@ function moveToShipSelect() {
         shipSelectButtons[i].addEventListener('click', () => {
             numberOfShips = i+1;
             makeRowsST(numberOfShips);
+            hoverMult();
+            placeAShip();
             backEnd = new Battleship( numberOfShips );
             moveToPlayerOnePlacementPrep();
         });
     }
 }
+
+function hoverMult(){
+    for ( let i = 0 ; i < 10 ; i ++ ){
+        for ( let j = 0 ; j < 10 ; j ++ ){
+            document.getElementById("P1main" + i + j ).addEventListener( "mouseover", function (){
+                if ( shipOrientation == "Horizontal" ){
+                    for ( let k = 0 ; k < shipSelected ; k ++ ){
+                        if ( j + k < 10 ){
+                            document.getElementById("P1main" + i + ( j + k ) ).className = 'grid-item-hovered';
+                        } else {
+                            for ( let l = shipSelected - k ; l > 0 ; l -- ){
+                                document.getElementById("P1main" + i + ( j - l ) ).className = 'grid-item-hovered';
+                            }
+                        }
+                    }
+                } else {
+                    for ( let k = 0 ; k < shipSelected ; k ++ ){
+                        if ( i + k < 10 ){
+                            document.getElementById("P1main" + ( i + k ) + j ).className = 'grid-item-hovered';
+                        } else {
+                            for ( let l = shipSelected - k ; l > 0 ; l -- ){
+                                document.getElementById("P1main" + ( i - l ) + j ).className = 'grid-item-hovered';
+                            }
+                        }
+                    }
+                }
+            });
+            document.getElementById("P1main" + i + j ).addEventListener( "mouseout", function (){
+                if ( shipOrientation == "Horizontal" ){
+                    for ( let k = 0 ; k < shipSelected ; k ++ ){
+                        if ( j + k < 10 ){
+                            document.getElementById("P1main" + i + ( j + k ) ).className = 'grid-item';
+                        } else {
+                            for ( let l = shipSelected - k ; l > 0 ; l -- ){
+                                document.getElementById("P1main" + i + ( j - l ) ).className = 'grid-item';
+                            }
+                        }
+                    }
+                } else {
+                    for ( let k = 0 ; k < shipSelected ; k ++ ){
+                        if ( i + k < 10 ){
+                            document.getElementById("P1main" + ( i + k ) + j ).className = 'grid-item';
+                        } else {
+                            for ( let l = shipSelected - k ; l > 0 ; l -- ){
+                                document.getElementById("P1main" + ( i - l ) + j ).className = 'grid-item';
+                            }
+                        }
+                    }
+                }
+            });
+        }
+    }
+}
+
+function placeAShip(){
+    for ( let i = 0 ; i < 10 ; i ++ ){
+        for ( let j = 0 ; j < 10 ; j ++ ){
+            document.getElementById("P1main" + i + j ).addEventListener( "click", function (){
+                if ( shipOrientation == "Horizontal" ){
+                    for ( let k = 0 ; k < shipSelected ; k ++ ){
+                        if ( j + k < 10 ){
+                            document.getElementById("P1main" + i + ( j + k ) ).className = 'grid-item-ship';
+                        } else {
+                            for ( let l = shipSelected - k ; l > 0 ; l -- ){
+                                document.getElementById("P1main" + i + ( j - l ) ).className = 'grid-item-ship';
+                            }
+                        }
+                    }
+                } else {
+                    for ( let k = 0 ; k < shipSelected ; k ++ ){
+                        if ( i + k < 10 ){
+                            document.getElementById("P1main" + ( i + k ) + j ).className = 'grid-item-ship';
+                        } else {
+                            for ( let l = shipSelected - k ; l > 0 ; l -- ){
+                                document.getElementById("P1main" + ( i - l ) + j ).className = 'grid-item-ship';
+                            }
+                        }
+                    }
+                }
+            }, { once : true });
+        }
+    }
+}
+
 
 function moveToPlayerOnePlacementPrep() {
     document.getElementById("shipselect").style.visibility = "hidden";
