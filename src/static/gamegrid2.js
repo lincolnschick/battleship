bbb35295366ca6ea6152381e8f74c8b23b640c9e
BayleyD <Bayley.D021001@gmail.com>
2022-02-07 13:25:46 -0600

    Added comments to gamegrid2.js

--- src/static/gamegrid2.js
+++ src/static/gamegrid2.js
@@ -1,5 +1,8 @@
 const container2 = document.getElementById("container2");
 
+//Creates the elements that represent each cell in the playerOne ghost board (guessing board)
+//Does so by setting it to a style, then going through creating each individual cell
+//After which it adds some basic functionality (hovering over a grid for guesses). 
 function makeRows2(rows, cols) {
   container2.style.setProperty('--grid-rows', rows);
   container2.style.setProperty('--grid-cols', cols);
